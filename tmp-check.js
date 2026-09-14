e88c7730dd95e34478e3ed69f2d2c2c44333219a
Tyson <tysonshields00@gmail.com>
2026-09-14 11:59:18 -0500

    chore: remove temporary utility scripts and update dashboard UI files

--- tmp-check.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const base = 'c:/Users/tyson/Documents/GitHub/Tyson-Shields-Personal-Website/';
-for (const f of ['about.html','career.html','skills.html','contact.html','404.html']) {
-  const t = fs.readFileSync(base + f, 'utf8');
-  const hasDrawer = t.includes('settings-drawer');
-  const tail = t.slice(-400);
-  console.log('--- ' + f + ' drawer=' + hasDrawer + ' ---');
-  console.log(tail.replace(/\r?\n/g, ' ').slice(-350));
-}
